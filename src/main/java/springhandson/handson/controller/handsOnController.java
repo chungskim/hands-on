292af5e7b7a84750d4509c0168cbe63fb3b347c3
chungskim <cskim2028@gmail.com>
2024-03-22 14:38:06 +0900

    api model

--- src/main/java/springhandson/handson/controller/handsOnController.java
+++ src/main/java/springhandson/handson/controller/handsOnController.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class handsOnController {
+
+    //Need Resource Static or Template
+
     @GetMapping("main") // url + /main 주소로 들어오면 해당 메소드로 호출
     public String mainpage(Model model){
         model.addAttribute("data", "This is main page");
@@ -19,5 +23,34 @@ public class handsOnController {
         // url + ?name='data'
         model.addAttribute("name", name);
         return "mvc-main-template";
+    }
+
+    // No Resource Template
+    // Use @ResponseBody
+
+    @GetMapping("html-body-main")
+    @ResponseBody
+    public String apiMainPage(@RequestParam("name") String name ) {
+        return name + " main"; // html body 에 데이터 삽입
+    }
+
+    @GetMapping("api-main")
+    @ResponseBody
+    public Main mainApi(@RequestParam("name") String name) {
+        Main main = new Main();
+        main.setName(name);
+        return main; // html body 에 Main 클래스의 field 데이터 삽입 (JSON)
+    }
+    // class for api model
+    static class Main { // Main 클래스 (객체)
+        private String name; // Main 클래스의 field 선언
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) { // Main 클래스의 메소드 선언
+            this.name = name;
+        }
     }
 }
